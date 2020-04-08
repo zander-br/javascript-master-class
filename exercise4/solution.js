50a4975d3e440fd0f35eaddbdc414227287c9057
Anderson Santos <anderson.feitoza.santos@gmail.com>
2020-04-08 07:42:14 -0300

    insert solution exercise 4

---
+++ exercise4/solution.js
@@ -0,0 +1,45 @@
+const statement = 'create table author (id number, name string, age number, city string, state string, country string)';
+
+const DatabaseError =  function(statement, message) {
+  this.statement = statement;
+  this.message = message;
+}
+
+const database = {
+  tables: {},
+  createTable(statement) {
+    const tableRegex = /table\s(\w+)/g;
+    const columnsRegex = /\((\D+)\)/g;
+    
+    const resultColumns = columnsRegex.exec(statement);
+    
+    const [_, tableName] = tableRegex.exec(statement);
+    const columns = resultColumns[1].split(', ');
+
+    this.tables = {[tableName]: {
+      columns: {},
+      data: [],
+    }};
+
+    for(let column of columns) {
+      const [name, type] = column.split(' ');
+      this.tables[tableName].columns[name] = type;
+    }
+  },
+  execute(statement) {
+    if(statement.startsWith('create table')) {
+      return this.createTable(statement);
+    }
+
+    const message = `Syntax error: '${statement}'`;
+    throw new DatabaseError(statement, message);
+  }
+};
+
+try {
+  database.execute("create table author (id number, name string, age number, city string, state string, country string)");
+  database.execute("select id, name from author");
+  console.log(JSON.stringify(database, undefined, "  ")); 
+} catch (e) {
+  console.log(e.message)
+}
